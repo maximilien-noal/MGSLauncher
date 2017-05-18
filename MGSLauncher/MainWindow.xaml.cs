58c334f382e00246490de7038c34da8039abfd50
Maximilien Noal <noal.maximilien@gmail.com>
2017-05-18 22:52:10 +0200

    git init

--- MGSLauncher/MainWindow.xaml.cs
+++ MGSLauncher/MainWindow.xaml.cs
@@ -1,27 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MGSLauncher
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public DelegateCommand Play { get; private set; }
+        public DelegateCommand PlayVR { get; private set; }
+        public DelegateCommand Quit { get; private set; }
+
         public MainWindow()
         {
+            Play = new DelegateCommand(Play_Execute);
+            PlayVR = new DelegateCommand(PlayVR_Execute);
+            Quit = new DelegateCommand(Quit_Execute);
+
             InitializeComponent();
+        }
+
+        private void Quit_Execute(object parameters)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void PlayVR_Execute(object parameters)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "MGSVR.EXE"));
+                Application.Current.Shutdown();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+        }
+
+        private void Play_Execute(object parameters)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "MGSI.EXE"));
+                Application.Current.Shutdown();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
         }
     }
 }
