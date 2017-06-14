96534631d3c8264bcf83382cd23a4be43b6d89cb
Maximilien Noal <noal.maximilien@gmail.com>
2017-06-14 22:09:57 +0200

    Hooks installed

--- Cutscene/Cutscene.cpp
+++ Cutscene/Cutscene.cpp
@@ -1,5 +1,6 @@
 // Cutscene.cpp : Defines the exported functions for the DLL application.
 //
+#include <Windows.h>
 #include <dshow.h>
 #include <strsafe.h>
 #include <vmr9.h>
@@ -20,17 +21,19 @@
 //
 #define KEYBOARD_SAMPLE_FREQ  100  // Sample user input on an interval
 
-IGraphBuilder  *pGB = NULL;
-IMediaControl  *pMC = NULL;
-IMediaEventEx  *pME = NULL;
-IBasicAudio    *pBA = NULL;
+IGraphBuilder	*pGB = NULL;
+IMediaControl	*pMC = NULL;
+IMediaEventEx	*pME = NULL;
+IBasicAudio		*pBA = NULL;
+RECT			g_rcDest = { 0 };
 
 // VMR9 interfaces
-IVMRWindowlessControl9 *pVW = NULL;
+IVMRWindowlessControl9 *pWC = NULL;
 
-DWORD g_dwGraphRegister = 0;
-HWND ghApp = 0;
-PLAYSTATE g_psCurrent = Stopped;
+DWORD		g_dwGraphRegister = 0;
+HWND		gameWindow = 0;
+PLAYSTATE	g_psCurrent = Stopped;
+BOOL		hookedWndProc = false;
 
 //
 // Function prototypes
@@ -53,12 +56,157 @@ HRESULT InitializeWindowlessVMR(IBaseFilter **ppVmr9);
 #define LIF(x) if (FAILED(hr=(x))) \
     {Msg(TEXT("FAILED(hr=0x%x) in ") TEXT(#x) TEXT("\n\0"), hr); return hr;}
 
+void OnPaint(HWND hwnd)
+{
+	HRESULT hr;
+	PAINTSTRUCT ps;
+	HDC         hdc;
+	RECT        rcClient;
+
+	GetClientRect(hwnd, &rcClient);
+	hdc = BeginPaint(hwnd, &ps);
+
+	if (pWC)
+	{
+		// When using VMR Windowless mode, you must explicitly tell the
+		// renderer when to repaint the video in response to WM_PAINT
+		// messages.  This is most important when the video is stopped
+		// or paused, since the VMR won't be automatically updating the
+		// window as the video plays.
+		hr = pWC->RepaintVideo(hwnd, hdc);
+	}
+	else  // No video image. Just paint the whole client area.
+	{
+		FillRect(hdc, &rcClient, (HBRUSH)(COLOR_BTNFACE + 1));
+	}
+
+	EndPaint(hwnd, &ps);
+}
+
+void MoveVideoWindow(void)
+{
+	HRESULT hr;
+
+	// Track the movement of the container window and resize as needed
+	if (pWC)
+	{
+		GetClientRect(gameWindow, &g_rcDest);
+		hr = pWC->SetVideoPosition(NULL, &g_rcDest);
+	}
+}
+
+HRESULT HandleGraphEvent(void)
+{
+	LONG evCode;
+	LONG_PTR evParam1, evParam2;
+	HRESULT hr = S_OK;
+
+	// Make sure that we don't access the media event interface
+	// after it has already been released.
+	if (!pME)
+		return S_OK;
+
+	// Process all queued events
+	while (SUCCEEDED(pME->GetEvent(&evCode, &evParam1, &evParam2, 0)))
+	{
+		// Free memory associated with callback, since we're not using it
+		hr = pME->FreeEventParams(evCode, evParam1, evParam2);
+	}
+
+	return hr;
+}
+
+static LRESULT CALLBACK CutsceneWndProc(UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		case WM_PAINT:
+			OnPaint(gameWindow);
+			break;
+
+		case WM_DISPLAYCHANGE:
+			if (pWC)
+				pWC->DisplayModeChanged();
+			break;
+
+		// Resize the video when the window changes
+		case WM_MOVE:
+		case WM_SIZE:
+			MoveVideoWindow();
+			break;
+
+		case WM_KEYDOWN:
+			CloseClip();
+			break;
+
+		case WM_GRAPHNOTIFY:
+			HandleGraphEvent();
+			break;
+		default:
+			return CallWindowProc((WNDPROC)GetWindowLongPtr(gameWindow, GWLP_WNDPROC), gameWindow, message, wParam, lParam);
+	}
+
+	return CallWindowProc((WNDPROC)GetWindowLongPtr(gameWindow, GWLP_WNDPROC), gameWindow, message, wParam, lParam);
+}
+
+
+
+void ErrorExit(LPTSTR lpszFunction)
+{
+	// Retrieve the system error message for the last-error code
+
+	LPVOID lpMsgBuf;
+	LPVOID lpDisplayBuf;
+	DWORD dw = GetLastError();
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		dw,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf,
+		0, NULL);
+
+	// Display the error message and exit the process
+
+	lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+		(lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
+	StringCchPrintf((LPTSTR)lpDisplayBuf,
+		LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+		TEXT("%s failed with error %d: %s"),
+		lpszFunction, dw, lpMsgBuf);
+	MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
+
+	LocalFree(lpMsgBuf);
+	LocalFree(lpDisplayBuf);
+	ExitProcess(dw);
+}
+
+HMODULE GetCurrentModuleHandle()
+{
+	HMODULE hMod = NULL;
+	GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+		reinterpret_cast<LPCWSTR>(&GetCurrentModuleHandle), &hMod);
+	return hMod;
+}
 
 
 HRESULT PlayVideo(LPTSTR szMovie, HINSTANCE processHandle, HWND gameWindow)
 {
 	HRESULT hr;
-	ghApp = gameWindow;
+	gameWindow = gameWindow;
+
+	if (hookedWndProc == false)
+	{
+		HHOOK hook = SetWindowsHookEx(WH_CALLWNDPROC, (HOOKPROC)&CutsceneWndProc, GetCurrentModuleHandle(), 0);
+		if (hook == NULL)
+		{
+			ErrorExit(L"SetWindowsHookEx");
+		}
+		hookedWndProc = true;
+	}
 
 	// Get the interface for DirectShow's GraphBuilder
 	JIF(CoCreateInstance(CLSID_FilterGraph, NULL, CLSCTX_INPROC_SERVER,
@@ -82,13 +230,13 @@ HRESULT PlayVideo(LPTSTR szMovie, HINSTANCE processHandle, HWND gameWindow)
 		JIF(pGB->QueryInterface(IID_IBasicAudio, (void **)&pBA));
 
 		// Have the graph signal event via window callbacks for performance
-		JIF(pME->SetNotifyWindow((OAHWND)ghApp, WM_GRAPHNOTIFY, 0));
+		JIF(pME->SetNotifyWindow((OAHWND)gameWindow, WM_GRAPHNOTIFY, 0));
 
 		// Complete initialization
-		ShowWindow(ghApp, SW_SHOWNORMAL);
-		UpdateWindow(ghApp);
-		SetForegroundWindow(ghApp);
-		SetFocus(ghApp);
+		ShowWindow(gameWindow, SW_SHOWNORMAL);
+		UpdateWindow(gameWindow);
+		SetForegroundWindow(gameWindow);
+		SetFocus(gameWindow);
 
 		hr = AddGraphToRot(pGB, &g_dwGraphRegister);
 		if (FAILED(hr))
@@ -101,7 +249,7 @@ HRESULT PlayVideo(LPTSTR szMovie, HINSTANCE processHandle, HWND gameWindow)
 		JIF(pMC->Run());
 		g_psCurrent = Running;
 
-		SetFocus(ghApp);
+		SetFocus(gameWindow);
 	}
 
 	return hr;
@@ -131,8 +279,8 @@ void CloseClip()
 
 	// Reset the player window
 	RECT rect;
-	GetClientRect(ghApp, &rect);
-	InvalidateRect(ghApp, &rect, TRUE);
+	GetClientRect(gameWindow, &rect);
+	InvalidateRect(gameWindow, &rect, TRUE);
 }
 
 
@@ -157,8 +305,6 @@ void CloseInterfaces(void)
 	SAFE_RELEASE(pGB);
 }
 
-
-
 HRESULT InitializeWindowlessVMR(IBaseFilter **ppVmr9)
 {
 	IBaseFilter* pVmr = NULL;
@@ -182,11 +328,11 @@ HRESULT InitializeWindowlessVMR(IBaseFilter **ppVmr9)
 			MIF(pVmr->QueryInterface(IID_IVMRFilterConfig9, (void**)&pConfig));
 			MIF(pConfig->SetRenderingMode(VMR9Mode_Windowless));
 
-			hr = pVmr->QueryInterface(IID_IVMRWindowlessControl9, (void**)&pVW);
+			hr = pVmr->QueryInterface(IID_IVMRWindowlessControl9, (void**)&pWC);
 			if (SUCCEEDED(hr))
 			{
-				MIF(pVW->SetVideoClippingWindow(ghApp));
-				MIF(pVW->SetBorderColor(RGB(0, 0, 0)));
+				MIF(pWC->SetVideoClippingWindow(gameWindow));
+				MIF(pWC->SetBorderColor(RGB(0, 0, 0)));
 			}
 		}
 
@@ -216,10 +362,31 @@ void Msg(TCHAR *szFormat, ...)
 	// Ensure that the formatted string is NULL-terminated
 	szBuffer[LASTCHAR] = TEXT('\0');
 
+	LPVOID lpMsgBuf;
+	LPVOID lpDisplayBuf;
+	DWORD dw = GetLastError();
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		dw,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf,
+		0, NULL);
+
+	lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+		(lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)L"") + 40) * sizeof(TCHAR));
+	StringCchPrintf((LPTSTR)lpDisplayBuf,
+		LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+		TEXT("%d: %s"),
+		L"", dw, lpMsgBuf);
+
 	// This sample uses a simple message box to convey warning and error
 	// messages.   You may want to display a debug string or suppress messages
 	// altogether, depending on your application.
-	MessageBox(NULL, szBuffer, TEXT("PlayVideo Error"), MB_OK);
+	MessageBox(NULL, szBuffer, (LPCWSTR)lpDisplayBuf, MB_OK);
 }
 
 
