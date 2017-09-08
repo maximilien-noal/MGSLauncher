e611d1cc3e3a327148062d97f633eee496e3fbdd
Maximilien Noal <maxim@max-laptop.(none)>
2017-09-08 22:40:19 +0200

    added more options to launcher

--- MGSLauncher/MainWindow.xaml.cs
+++ MGSLauncher/MainWindow.xaml.cs
@@ -1,37 +1,79 @@
-﻿using MGSHook;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
 using System.Windows;
-using System.Windows.Interop;
 
 namespace MGSLauncher
 {
     public partial class MainWindow : Window
     {
-        public bool IsDgVoodoo2Deactivated
+        public bool AreOptionsNotVisible
         {
-            get { return (bool)GetValue(IsDgVoodoo2DeactivatedProperty); }
-            set { SetValue(IsDgVoodoo2DeactivatedProperty, value); }
+            get { return (bool)GetValue(AreOptionsNotVisibleProperty); }
+            set { SetValue(AreOptionsNotVisibleProperty, value); }
         }
-        public static readonly DependencyProperty IsDgVoodoo2DeactivatedProperty =
-            DependencyProperty.Register("IsDgVoodoo2Deactivated", typeof(bool), typeof(MainWindow), new PropertyMetadata(Properties.Settings.Default.IsDgVoodoo2Deactivated));
+        public static readonly DependencyProperty AreOptionsNotVisibleProperty =
+            DependencyProperty.Register("AreOptionsNotVisible", typeof(bool), typeof(MainWindow), new PropertyMetadata(true));
+
+        public bool AreOptionsVisible
+        {
+            get { return (bool)GetValue(AreOptionsVisibleProperty); }
+            set { SetValue(AreOptionsVisibleProperty, value); }
+        }
+        public static readonly DependencyProperty AreOptionsVisibleProperty =
+            DependencyProperty.Register("AreOptionsVisible", typeof(bool), typeof(MainWindow), new PropertyMetadata(false));
+
+        public bool IsDgVoodoo2Activated
+        {
+            get { return (bool)GetValue(IsDgVoodoo2ActivatedProperty); }
+            set { SetValue(IsDgVoodoo2ActivatedProperty, value); }
+        }
+        public static readonly DependencyProperty IsDgVoodoo2ActivatedProperty =
+            DependencyProperty.Register("IsDgVoodoo2Activated", typeof(bool), typeof(MainWindow), new PropertyMetadata(Properties.Settings.Default.IsDgVoodoo2Activated));
+
+        public bool IsVideoFixActivated
+        {
+            get { return (bool)GetValue(IsVideoFixActivatedProperty); }
+            set { SetValue(IsVideoFixActivatedProperty, value); }
+        }
+        public static readonly DependencyProperty IsVideoFixActivatedProperty =
+            DependencyProperty.Register("IsVideoFixActivated", typeof(bool), typeof(MainWindow), new PropertyMetadata(Properties.Settings.Default.IsVideoFixActivated));
+
+        public bool IsSweetFXActivated
+        {
+            get { return (bool)GetValue(IsSweetFXActivatedProperty); }
+            set { SetValue(IsSweetFXActivatedProperty, value); }
+        }
+        public static readonly DependencyProperty IsSweetFXActivatedProperty =
+            DependencyProperty.Register("IsSweetFXActivated", typeof(bool), typeof(MainWindow), new PropertyMetadata(Properties.Settings.Default.IsSweetFXActivated));
 
         public DelegateCommand Play { get; private set; }
         public DelegateCommand PlayVR { get; private set; }
         public DelegateCommand Quit { get; private set; }
+        public DelegateCommand ShowOptions { get; private set; }
 
         public MainWindow()
         {
             Play = new DelegateCommand(Play_Execute);
             PlayVR = new DelegateCommand(PlayVR_Execute);
             Quit = new DelegateCommand(Quit_Execute);
+            ShowOptions = new DelegateCommand(ShowOptions_Execute);
 
             InitializeComponent();
+
+            Application.Current.Exit += OnApplicationExit;
+        }
+
+        private void OnApplicationExit(object sender, ExitEventArgs e)
+        {
+            SaveOptions();
+        }
+
+        private void SaveOptions()
+        {
+            Properties.Settings.Default.IsDgVoodoo2Activated = IsDgVoodoo2Activated;
+            Properties.Settings.Default.IsVideoFixActivated = IsVideoFixActivated;
+            Properties.Settings.Default.IsSweetFXActivated = IsSweetFXActivated;
+            Properties.Settings.Default.Save();
         }
 
         private void Quit_Execute(object parameters)
@@ -41,29 +83,50 @@ namespace MGSLauncher
 
         private void PlayVR_Execute(object parameters)
         {
+            LaunchGame("MGSVR.EXE");
+        }
+
+        private void Play_Execute(object parameters)
+        {
+            LaunchGame("MGSI.EXE");
+        }
+
+        private void LaunchGame(string executable)
+        {
             try
             {
-                HookLauncher.Run("MGSVR.EXE");
-                this.WindowState = WindowState.Minimized;
+                SaveOptions();
+                OptionsActivator.ApplyOptions();
+
+                if (IsVideoFixActivated)
+                {
+                    HookLauncher.Run(executable);
+                    this.WindowState = WindowState.Minimized;
+                }
+                else
+                {
+                    Process.Start(executable);
+                    Application.Current.Shutdown();
+                }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 this.WindowState = WindowState.Normal;
                 Debug.WriteLine(e.Message);
             }
         }
 
-        private void Play_Execute(object parameters)
+        private void ShowOptions_Execute(object parameters)
         {
-            try
+            if(AreOptionsNotVisible)
             {
-                HookLauncher.Run("MGSI.EXE");
-                this.WindowState = WindowState.Minimized;
+                SetCurrentValue(AreOptionsVisibleProperty, true);
+                SetCurrentValue(AreOptionsNotVisibleProperty, false);
             }
-            catch(Exception e)
+            else
             {
-                this.WindowState = WindowState.Normal;
-                Debug.WriteLine(e.Message);
+                SetCurrentValue(AreOptionsVisibleProperty, false);
+                SetCurrentValue(AreOptionsNotVisibleProperty, true);
             }
         }
     }
---
+++ MGSLauncher/OptionsActivator.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows;
+
+namespace MGSLauncher
+{
+    internal static class OptionsActivator
+    {
+        private static string _gameDir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+        public static void ApplyOptions()
+        {
+            if(Properties.Settings.Default.IsDgVoodoo2Activated)
+            {
+                EnableDgVoodoo2();
+            }
+            else
+            {
+                DisableDgVoodoo2();
+            }
+
+            if(Properties.Settings.Default.IsSweetFXActivated)
+            {
+                EnableSweetFX();
+            }
+            else
+            {
+                DisableSweetFX();
+            }
+        }
+
+        private static void EnableDgVoodoo2()
+        {
+            try
+            {
+                string saveDgVoodoo2DirPath = Path.Combine(_gameDir, "dgvoodoo2");
+
+                string ddrawPath = Path.Combine(_gameDir, "ddraw.dll");
+                string ddrawSavePath = Path.Combine(saveDgVoodoo2DirPath, "ddraw.dll");
+                string d3DImmPath = Path.Combine(_gameDir, "D3DImm.dll");
+                string d3DImmSavePath = Path.Combine(saveDgVoodoo2DirPath, "D3DImm.dll");
+                string d3d8Path = Path.Combine(_gameDir, "d3d8.dll");
+                string d3d8SavePath = Path.Combine(saveDgVoodoo2DirPath, "d3d8.dll");
+
+                if (File.Exists(ddrawSavePath) && File.Exists(ddrawPath) == false)
+                {
+                    File.Move(ddrawSavePath, ddrawPath);
+                }
+
+                if (File.Exists(d3DImmSavePath) && File.Exists(d3DImmPath) == false)
+                {
+                    File.Move(d3DImmSavePath, d3DImmPath);
+                }
+
+                if (File.Exists(d3d8SavePath) && File.Exists(d3d8Path) == false)
+                {
+                    File.Move(d3d8SavePath, d3d8Path);
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(String.Format("{0}:{1}{2}", e.Message, Environment.NewLine, e.StackTrace), "Error enabling dgVoodoo2");
+            }
+        }
+
+        private static void DisableDgVoodoo2()
+        {
+            try
+            {
+                string saveDgVoodoo2DirPath = Path.Combine(_gameDir, "dgvoodoo2");
+                if(Directory.Exists(saveDgVoodoo2DirPath) == false)
+                {
+                    Directory.CreateDirectory(saveDgVoodoo2DirPath);
+                }
+
+                string ddrawPath = Path.Combine(_gameDir, "ddraw.dll");
+                string ddrawSavePath = Path.Combine(saveDgVoodoo2DirPath, "ddraw.dll");
+                string d3DImmPath = Path.Combine(_gameDir, "D3DImm.dll");
+                string d3DImmSavePath = Path.Combine(saveDgVoodoo2DirPath, "D3DImm.dll");
+                string d3d8Path = Path.Combine(_gameDir, "d3d8.dll");
+                string d3d8SavePath = Path.Combine(saveDgVoodoo2DirPath, "d3d8.dll");
+
+                if(File.Exists(ddrawPath))
+                {
+                    if (File.Exists(ddrawSavePath))
+                    {
+                        File.Delete(ddrawSavePath);
+                    }
+                    File.Move(ddrawPath, ddrawSavePath);
+                }
+
+                if (File.Exists(d3DImmPath))
+                {
+                    if (File.Exists(d3DImmSavePath))
+                    {
+                        File.Delete(d3DImmSavePath);
+                    }
+                    File.Move(d3DImmPath, d3DImmSavePath);
+                }
+
+                if (File.Exists(d3d8Path))
+                {
+                    if (File.Exists(d3d8SavePath))
+                    {
+                        File.Delete(d3d8SavePath);
+                    }
+                    File.Move(d3d8Path, d3d8SavePath);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(String.Format("{0}:{1}{2}", e.Message, Environment.NewLine, e.StackTrace), "Error disabling dgVoodoo2");
+            }
+        }
+
+        private static void EnableSweetFX()
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(String.Format("{0}:{1}{2}", e.Message, Environment.NewLine, e.StackTrace), "Error enabling SweetFX");
+            }
+        }
+
+        private static void DisableSweetFX()
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(String.Format("{0}:{1}{2}", e.Message, Environment.NewLine, e.StackTrace), "Error disabling SweetFX");
+            }
+        }
+    }
+}
